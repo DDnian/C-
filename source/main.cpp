cc8234816d2967e2e3f15aae04477a6b7adc4dba
DDnian <3042573380@qq.com>
2024-05-02 15:06:14 +0800

    暂存配置

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "add.h"
+using namespace std;
+int main(){
+    int a = 4;
+    int b = 8;
+    int c = add(a, b);
+    cout << c << endl;
+    system("pause");
+    return 0;
+}
