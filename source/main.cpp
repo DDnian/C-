0b58609e0ef3999da16263ea5545204caa9908d4
DDnian <3042573380@qq.com>
2024-05-02 15:46:09 +0800

    测试一下调试功能

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,33 @@ int main()
     string name;
     cin >> name;
     cout << name << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << name << endl;
+    }
+    int chance;
+    cin >> chance;
+    switch (chance)
+    {
+    case 1:
+        cout << "hello world" << endl;
+        break;
+    case 2:
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                cout << name << endl;
+            }
+        }
+        break;
+    case 3:
+        cout << "xiaosishushule" << endl;
+        break;
+    default:
+        cout << "haha" << endl;
+    }
+    
     system("pause");
     return 0;
 }
